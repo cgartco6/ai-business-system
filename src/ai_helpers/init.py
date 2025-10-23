c48995c60ce8b0ba181755536215c433dae60511
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-10-23 10:09:31 +0200

    Create init.py

---
+++ src/ai_helpers/init.py
@@ -0,0 +1,6 @@
+from .email_manager import EmailManager
+from .database_handler import DatabaseHandler
+from .api_client import APIClient
+from .report_generator import ReportGenerator
+
+__all__ = ['EmailManager', 'DatabaseHandler', 'APIClient', 'ReportGenerator']
