0a52c4d172f8762ccea891f7fb03951aceccea8b
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-10-23 10:04:43 +0200

    Create init.py

---
+++ src/ai_agents/init.py
@@ -0,0 +1,6 @@
+from .lead_generator import LeadGenerator
+from .outreach_agent import OutreachAgent
+from .research_agent import ResearchAgent
+from .monitor_agent import MonitorAgent
+
+__all__ = ['LeadGenerator', 'OutreachAgent', 'ResearchAgent', 'MonitorAgent']
